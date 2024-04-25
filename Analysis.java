895b7fac5542d0dc6c602d1e90e4ca9986aebdc4
uzair223 <uzairpatel223@outlook.com>
2024-04-25 16:51:02 +0100

    Implementing file input argument

--- Analysis.java
+++ Analysis.java
@@ -1,12 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /*
  * SCC Social Networks Summer Project
 */
 public class Analysis {
   
   public static void main(String[] args) {
-    String inputString = "Raman Abe Carla Anwar Cat\nCat Abe\nCarla Raman Abe\nAbe\nAnwar Abe Carla Raman";
-    Graph<String> network = new NetworkParser(inputString).parse();
-    
-    System.out.println(network.countVertices());
+    try {
+      File file = new File(args[0]);
+      Scanner reader = new Scanner(file);
+      Graph<String> network = new NetworkParser(reader.useDelimiter("\n").tokens()).parse();
+      reader.close();
+      System.out.println(network.countVertices());
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occured");
+      e.printStackTrace();
+    }
    }
 }
--- NetworkParser.java
+++ NetworkParser.java
@@ -1,21 +1,23 @@
 import java.util.*;
+import java.util.stream.Stream;
 /*
  * Class to parse string into graph
  */
 public class NetworkParser {
-  String rawString;
+  Stream<String> lineStream;
 
-  NetworkParser(String rawString) {
-    this.rawString = rawString;
+  NetworkParser(Stream<String> lineStream) {
+    this.lineStream = lineStream;
   }
   public Graph<String> parse(){
     Graph<String> graph = new Graph<>();
-    rawString.lines().forEach(line -> {
+    lineStream.forEach(line -> {
       ArrayList<String> tokens = new ArrayList<String>(Arrays.asList(line.split(" ")));
       if(tokens.size() < 1) return;
       String from = tokens.remove(0);
-      for(String vertex : tokens) {
-        graph.addDirectedEdge(from, vertex);
+      for(String token : tokens) {
+        String vertex = token.strip();
+        if(vertex.length() > 0) graph.addDirectedEdge(from,vertex);
       }
     });
     return graph;
